bbe4de70f0e281efd8eb718995767a084b74b91d
musa_dv <musa_dv@abv.bg>
2022-03-27 17:20:55 +0300

    add login component

---
+++ src/components/Login.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import { useContext } from 'react'
+import { UserContext } from './UserContext';
+
+ function Login() {
+
+ 
+
+ 
+
+  return (
+    <div className='login-form'>
+      <form action="#">
+          <label htmlFor="userName">Потребителско име</label>
+          <input type="username" placeholder='your username' />
+      </form>
+    </div>
+  )
+}
+
+export default Login
